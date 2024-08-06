7fc34d11cedf7cfa2979f7714168f612ffb17264
Maye11223 <mymayerli11@gmail.com>
2024-08-06 10:46:44 -0500

    añadi conexion con el servidos con node.js por defecto y importe boostrap en la carpeta index.js

--- Gestion de ambientes/Coordinador/APP SENA/server.js
+++ Gestion de ambientes/Coordinador/APP SENA/server.js
@@ -17,7 +17,7 @@ app.listen(port, () => {
 //luz puedes configurar si no tienes... el sevidor local simple con node.js
 // installa node.js 
 //ejecuta estos comandos en la terminal de este proyecto: (npm init -y) y (npm install express)  
-//inicia el servidor con este comando : node server.js
+//inicia el servidor con este comando : node server.js (Ejecuta el servidor Node.js para servir la app frontend)
 // ya ahi el proyecto estaria disponible en http://localhost:3000 
 //o si lo estas haciendo en visual studio code instala la extencion live server
 //IMPORTANTE para correr tu codigo lo haces desde index.html clic derecho y ejecutar
