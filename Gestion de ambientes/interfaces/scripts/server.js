b4c494aa085bd7842368aede990934dcdf863cc7
Maye11223 <mymayerli11@gmail.com>
2024-08-30 10:21:08 -0500

    ajudtando redireccion

--- Gestion de ambientes/interfaces/scripts/server.js
+++ Gestion de ambientes/interfaces/scripts/server.js
@@ -10,14 +10,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
-//luz puedes configurar si no tienes... el sevidor local simple con node.js
-// installa node.js 
-//ejecuta estos comandos en la terminal de este proyecto: (npm init -y) y (npm install express)  
-//inicia el servidor con este comando : node server.js (Ejecuta el servidor Node.js para servir la app frontend)
-// ya ahi el proyecto estaria disponible en http://localhost:3000 
-//o si lo estas haciendo en visual studio code instala la extencion live server
-//IMPORTANTE para correr tu codigo lo haces desde index.html clic derecho y ejecutar o desde cmd :node.js
