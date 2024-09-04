2c7886447804cdbe2b37ee86af95b2e201024663
Maye11223 <mymayerli11@gmail.com>
2024-09-04 09:15:52 -0500

    ajustando crud en front para connectar con backend

--- Gestion de ambientes/interfaces/scripts/Login.js
+++ Gestion de ambientes/interfaces/scripts/Login.js
@@ -28,26 +28,28 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('http://localhost:8080/login', {  // URL completa del backend
+    fetch('http://localhost:8080/login', {//redirigi datos del formulario para ser auteticados por spring security y obtener por resuesta el rol para ser redirigido
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify({ username, password }),
+        body: new URLSearchParams({ username, password }),
     })
     .then(response => {
-        if (!response.ok) {
+        if (response.ok) {
+            return fetch('http://localhost:8080/login/success');
+        } else {
             throw new Error('Error en la respuesta del servidor');
         }
-        return response.json();
     })
+    .then(response => response.json())
     .then(data => {
         if (data.role === 'ROL_COORDINADOR') {
-            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/interfaces/coordinadorDashboard.html';
+            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/Coordinador/coordinadorDashboard.html';
         } else if (data.role === 'ROL_ALISTAMIENTO') {
-            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/interfaces/alistamientoDashboard.html';
+            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/Alistamiento/alistamientoDashboard.html';
         } else if (data.role === 'ROL_INSTRUCTOR') {
-            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/interfaces/instructorDashboard.html';
+            window.location.href = '/Gestion-de-Ambientes/Gestion%20de%20ambientes/Instructor/instructorDashboard.html';
         } else {
             alert('Rol no reconocido');
         }
@@ -57,4 +59,3 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert('Error al iniciar sesión');
     });
 });
-
