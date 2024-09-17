789a56be8187bb47b21ee82563ef8e37ce94c43e
Luz <lmsanchezalv@gmail.com>
2024-09-17 10:40:20 -0500

    js-calendarioSede

--- Gestion de ambientes/Coordinador/js/Elegir_Sede.js
+++ Gestion de ambientes/Coordinador/js/Elegir_Sede.js
@@ -7,3 +7,31 @@ if (calendar_central) {
     });
 }
 
+const calendar_pomar = document.getElementById('card-pomar');
+
+if (calendar_pomar) {
+  calendar_pomar.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href ='/Gestion de ambientes/Coordinador/Calendario/Calendario_sedePomar.html'; 
+    });
+}
+
+
+const calendar_buenosAires= document.getElementById('card-buenosAires');
+
+if (calendar_buenosAires) {
+  calendar_buenosAires.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href ='/Gestion de ambientes/Coordinador/Calendario/Calendario_sedeBuenosAires.html'; 
+    });
+}
+
+
+const calendar_usb = document.getElementById('card-usb');
+
+if (calendar_usb) {
+  calendar_usb.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href ='/Gestion de ambientes/Coordinador/Calendario/Calendario_sedeUSB.html'; 
+    });
+}
